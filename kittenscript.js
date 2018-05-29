8456b3ffface71ed401c904bc08ac8851df9437a
Ahlberger <lina.ahlberger@gmail.com>
2018-05-29 10:01:28 +0200

    Update kittenscript.js

--- kittenscript.js
+++ kittenscript.js
@@ -140,23 +140,10 @@ let bot;
                     var curRes = gamePage.resPool.get(resources[i][0]);
                     if (curRes.value / curRes.maxValue > 0.99 && gamePage.workshop.getCraft(resources[i][1]).unlocked)
                     {
-                        gamePage.craft(resources[i][1],10);
-                        gamePage.craft(resources[i][1],10);
-                        gamePage.craft(resources[i][1],10);
-                        gamePage.craft(resources[i][1],10);
-                        gamePage.craft(resources[i][1],10);
-                        gamePage.craft(resources[i][1],10);
-                    }
-                }
-                if(gamePage.resPool.get('slab').value > 100000000 && gamePage.resPool.get('concrate').unlocked)
-                    while(gamePage.resPool.get('slab').value > 99900000)
-                        gamePage.craft(gamePage.resPool.get('concrate').name,1);
-                var titan = gamePage.resPool.get('titanium');
-                var steel = gamePage.resPool.get('steel');
-                var coal = gamePage.resPool.get('coal');
-                if(titan.value/titan.maxValue>0.99 && steel.value/coal.maxValue>0.1 && gamePage.resPool.get('alloy').unlocked)
-                    gamePage.craft(gamePage.resPool.get('alloy'),10);
-            }, 2 * 1000);
+                        gamePage.craftAll(resources[i][1]);
+        }
+    }
+}, 5 * 1000);
         }
 
         pray()
